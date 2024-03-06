e4b6730344cc7698e79b7fea9189077973bc855d
GarrixA <aphrogarrix10@gmail.com>
2024-03-06 22:49:14 +0200

    tyscript exercises

---
+++ TS/public/sandbox.js
@@ -0,0 +1,8 @@
+"use strict";
+const myName = "Kavuyo";
+console.log(myName);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(item => {
+    console.log(item);
+});
